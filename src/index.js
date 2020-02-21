3f91ccf78912c226638ab7e2d12f27849bfc73ee
Brandon Weaver <a7xncob@gmail.com>
2020-02-21 23:19:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ window.addEventListener("DOMContentLoaded", function(event)
       {
         for (const breed in breeds)
         {
-          console.log(breed[0].toString());
+          console.log(breed);
           if (breed[0] == option)
           {
             const li = document.createElement("li");
