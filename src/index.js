98ed8d985c2e024a49521c2eb387b83521570e14
Brandon Weaver <a7xncob@gmail.com>
2020-02-21 23:35:54 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ window.addEventListener("DOMContentLoaded", function(event)
       {
         for (let i = 0; i < breeds.length; i++)
         {
-          if (breeds[i][0] == dropdown.options[i].innerText)
+          if (breeds[i][0] == document.getElementById("breed-dropdown").options[i].innerText)
           {
             const li = document.createElement("li");
             li.innerText = breeds[i];
