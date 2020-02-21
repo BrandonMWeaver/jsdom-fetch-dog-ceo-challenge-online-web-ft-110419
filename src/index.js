f4564ee2ec0a9c252ebb904b5ac89322f78235b1
Brandon Weaver <a7xncob@gmail.com>
2020-02-21 21:43:26 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ window.addEventListener("DOMContentLoaded", function(event) {
   window.fetch("https://dog.ceo/api/breeds/image/random/4").then(function(response) {
     return response.json();
   }).then(function(json) {
-    json[""]
+    for (let i = 0; i < json.length; i++)
+    {
+      console.log(json[i]);
+    }
   });
 });
